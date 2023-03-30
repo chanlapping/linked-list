9dffb11263139334e80896b470a7794bd1736fa3
chan lap ping <chanlapping64@gmail.com>
2023-03-30 21:27:07 +0800

    Add Node and LinkedList classes Add append and prepend functions

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ class Node {
     }
 }
 
+class LinkedList {
+    constructor(firstNode = null) {
+        this.firstNode = firstNode;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if (!this.firstNode) {
+            this.firstNode = newNode;
+            return;
+        }
+        let currentNode = this.firstNode;
+        while(currentNode.nextNode) {
+            currentNode = currentNode.nextNode;
+        }
+        currentNode.nextNode = newNode;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value, this.firstNode);
+        this.firstNode = newNode;
+    }
+}
+
+const list = new LinkedList();
+list.append(13);
+list.append(14);
+list.append(15);
+list.prepend(12);
+console.log(list);
