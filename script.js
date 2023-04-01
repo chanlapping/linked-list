e9d1eeb14c8530396fca4e7cb90040b8109efbac
chan lap ping <chanlapping64@gmail.com>
2023-04-01 11:02:53 +0800

    Add various methods to the LinkedList class

--- script.js
+++ script.js
@@ -16,16 +16,77 @@ class LinkedList {
             this.firstNode = newNode;
             return;
         }
-        let currentNode = this.firstNode;
-        while(currentNode.nextNode) {
-            currentNode = currentNode.nextNode;
-        }
-        currentNode.nextNode = newNode;
+        
+        this.tail().nextNode = newNode;
     }
 
     prepend(value) {
         const newNode = new Node(value, this.firstNode);
         this.firstNode = newNode;
+    }
+
+    size() {
+        let result = 0;
+        let currentNode = this.firstNode;
+        while (currentNode) {
+            result++;
+            currentNode = currentNode.nextNode;
+        }
+        return result;
+    }
+
+    head() {
+        return this.firstNode;
+    }
+
+    tail() {
+        let currentNode = this.firstNode;
+        while (currentNode.nextNode) {
+            currentNode = currentNode.nextNode;
+        }
+        return currentNode;
+    }
+
+    at(index) {
+        if (index >= this.size()) {
+            return null;
+        }
+        let i = 0;
+        let currentNode = this.firstNode;
+        while (i < index) {
+            currentNode = currentNode.nextNode;
+            i++;
+        }
+        return currentNode;
+    }
+
+    pop() {
+        const newTail = this.at(this.size() - 2);
+        const oldTail = newTail.nextNode;
+        newTail.nextNode = null;
+        return oldTail;
+    }
+
+    contains(value) {
+        let currentNode = this.firstNode;
+        while (currentNode) {
+            if (currentNode.value == value) {
+                return true;
+            }
+            currentNode = currentNode.nextNode;
+        }
+        return false;
+    }
+
+    toString() {
+        let result = '';
+        let currentNode = this.firstNode;
+        while (currentNode) {
+            result += `( ${currentNode.value} ) --> `;
+            currentNode = currentNode.nextNode;
+        }
+        result += 'null';
+        return result;
     }
 }
 
@@ -34,4 +95,6 @@ list.append(13);
 list.append(14);
 list.append(15);
 list.prepend(12);
-console.log(list);
+list.prepend(11);
+console.log(list.toString());
+console.log(list.contains(3));
